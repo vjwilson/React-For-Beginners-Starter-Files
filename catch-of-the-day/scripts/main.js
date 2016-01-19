ca8c13756ecfcfd60086c40b10982bdc07f86378
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-01-18 21:19:26 -0500

    Add more components to app

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -2,6 +2,72 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /**
+ * App
+ */
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
+});
+
+/**
+ * Header
+ */
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+        Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    );
+  }
+});
+
+/**
+ * Order
+ */
+
+var Order = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Order</p>
+      </div>
+    );
+  }
+});
+
+/**
+ * Inventory
+ */
+
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Inventory</p>
+      </div>
+    );
+  }
+});
+
+/**
  * StorePicker
  * This will let us make <StorePicker/>
  */
@@ -17,6 +83,6 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-})
+});
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
