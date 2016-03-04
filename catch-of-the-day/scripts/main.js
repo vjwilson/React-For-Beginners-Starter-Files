4438564f628a9f5fe712fb7afbbbda621193b79c
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-03-04 06:12:27 -0500

    Load data into state onClick, lesson 11

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -30,6 +30,11 @@ var App = React.createClass({
     // set the state object
     this.setState({ fishes : this.state.fishes });
   },
+  loadSamples: function() {
+    this.setState({
+      fishes: require('./sample-fishes')
+    });
+  },
   render: function() {
     return (
       <div className="catch-of-the-day">
@@ -37,7 +42,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     );
   }
@@ -124,6 +129,7 @@ var Inventory = React.createClass({
         <h2>Inventory</h2>
 
         <AddFishForm  {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     );
   }
