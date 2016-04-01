f00d272af5a14fce98833cdaac4169e975a66c1a
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-04-01 06:54:46 -0400

    Start OAuth login for Firebase

--- catch-of-the-day/scripts/components/Inventory.js
+++ catch-of-the-day/scripts/components/Inventory.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
-
+import Firebase from 'firebase';
+const ref = new Firebase('https://sizzling-inferno-7336.firebaseio.com/');
 import AddFishForm from './AddFishForm';
 
 /**
@@ -9,6 +10,34 @@ import AddFishForm from './AddFishForm';
 
 @autobind
 class Inventory extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      uid: ''
+    }
+  }
+
+  authenticate(provider) {
+    console.log('Trying to auth with ' + provider);
+    ref.authWithOAuthPopup(provider, function(err, authData) {
+      console.log(authData);
+    });
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manange your store's inventory</p>
+        <button className="github" onClick={this.authenticate.bind(this, 'github')}>Log In with Github</button>
+        <button className="facebook" onClick={this.authenticate.bind(this, 'facebook')}>Log In with Facebook</button>
+        <button className="twitter" onClick={this.authenticate.bind(this, 'twitter')}>Log In with Twitter</button>
+      </nav>
+    )
+  }
+
   renderInventory(key) {
     var linkState = this.props.linkState;
     return (
@@ -19,6 +48,7 @@ class Inventory extends React.Component {
           <option value="available">Fresh</option>
           <option value="unavailable">Sold Out!</option>
         </select>
+
         <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
         <input type="text" valueLink={linkState('fishes.' + key + '.image')}/>
         <button type="submit" onClick={this.props.removeFish.bind(null, key)}>Remove Item</button>
@@ -27,8 +57,28 @@ class Inventory extends React.Component {
   }
 
   render() {
+    let logoutButton = <button>Log Out!</button>
+
+    // first check if they are logged in
+    if (!this.state.uid) {
+      return(
+        <div>{this.renderLogin()}</div>
+      )
+    }
+
+    // then check if they aren't the owner of the current store
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of this store.</p>
+          {logoutButton}
+        </div>
+      )
+    }
+
     return (
       <div>
+        {logoutButton}
         <h2>Inventory</h2>
 
         {Object.keys(this.props.fishes).map(this.renderInventory)}
